6bdf7d58f62d0bbcbb1dbf91d1913013c086c29c
sulivan-dev <wil.alvarado.dev@gmail.com>
2021-02-18 22:05:49 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-movies/precache-manifest.c19982621624b43b752ac7edafb038a3.js"
+  "/my-movies/precache-manifest.7ff9eab797c94b7c3d049aee9226a9b9.js"
 );
 
 self.addEventListener('message', (event) => {

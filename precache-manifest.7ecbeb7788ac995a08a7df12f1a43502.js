ba8cd6997fdef8c5d268063ea76209aaf260299d
sulivan-dev <wil.alvarado.dev@gmail.com>
2021-02-18 21:50:12 -0600

    Updates

---
+++ precache-manifest.7ecbeb7788ac995a08a7df12f1a43502.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2164dec77a55725f3cdc2a5bf1a681",
+    "url": "/my-movies/index.html"
+  },
+  {
+    "revision": "f4b8d8f6426a2d63ea93",
+    "url": "/my-movies/static/css/2.bfcdb919.chunk.css"
+  },
+  {
+    "revision": "f4b8d8f6426a2d63ea93",
+    "url": "/my-movies/static/js/2.9358eddd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-movies/static/js/2.9358eddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "963fa0d31e8d64b3d39e",
+    "url": "/my-movies/static/js/main.9ebf2716.chunk.js"
+  },
+  {
+    "revision": "0adc94c6a42def3086ce",
+    "url": "/my-movies/static/js/runtime-main.78ffea68.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-movies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my-movies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-movies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-movies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-movies/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "08392f186e99a74794065effff6f90e5",
+    "url": "/my-movies/static/media/logo.08392f18.svg"
+  }
+]);
